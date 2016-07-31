b98c1e99f30d93f2bcb69e40f941b567df757f50
Pedro Melo <melo@simplicidade.org>
2016-07-31 23:06:24 +0100

    Read entries from MT DB and write the Hugo posts
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- movable_type2hugo.pl
+++ movable_type2hugo.pl
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 use DBI;
 use Getopt::Long;
+use Path::Tiny;
+use Time::Moment;
 
 sub usage {
   print "FATAL: @_\n\n" if @_;
@@ -47,21 +49,73 @@ sub convert_db_to_hugo {
   }
 }
 
+sub collect_posts_from_mt_db {
+  my $dbh = DBI->connect("dbi:SQLite:$opts{db}") or fatal("could not open DB '$opts{db}'");
+
+  return $dbh->selectall_arrayref(
+    q{
+    SELECT e.entry_basename, e.entry_keywords, e.entry_status, e.entry_text,
+           e.entry_text_more, e.entry_title, e.entry_created_on,
+           c.category_label
+      FROM mt_entry e
+           LEFT JOIN mt_category c ON (c.category_id=e.entry_category_id)
+     WHERE e.entry_blog_id=?
+  }, { Slice => {} }, $opts{blog}
+  );
+}
+
+sub generate_hugo_post {
+  my ($post) = @_;
+
+  my $date = eval { Time::Moment->from_string("$post->{entry_created_on}Z", lenient => 1) };
+  fatal("could not parse post date '$post->{entry_created_on}'") unless $date;
+
+  my $path =
+    path($opts{hugo_dir}, 'content', 'notes', $date->year, $date->strftime('%m'), "$post->{entry_basename}.md");
+  $path->parent->mkpath;
+  my $fh = $path->openw;
+
+  print $fh "+++\n";
+  print $fh 'title = ',       quote($post->{entry_title}) . "\n";
+  print $fh 'date = ',        quote($date->to_string), "\n";
+  print $fh 'categories = [', quote($post->{category_label}), "]\n" if $post->{category_label};
+
+  if (my $tags = $post->{entry_keywords}) {
+    print $fh "tags = [", join(', ', map { quote($_) } split(m/[,\s]+/, $tags)), "]\n";
+  }
+
+  print $fh 'alias = ',
+    quote(join('/', qw( notes archive ), $date->year, $date->strftime('%m'), "$post->{entry_basename}.html")), "\n";
+  print $fh "draft = true\n" if $post->{entry_status} != 2;
+  print $fh "+++\n\n";
+
+  print $fh $post->{entry_text},      "\n\n" if $post->{entry_text};
+  print $fh $post->{entry_text_more}, "\n\n" if $post->{entry_text_more};
+
+  $fh->close();
+}
+
+sub quote {
+  my ($t) = @_;
+  $t =~ s/"/\\"/g;
+  return qq{"$t"};
+}
 
 #################
 # Options parsing
 
 sub parse_options {
-  my %opts;
+  my %opts = (blog => 1);
 
-  GetOptions(\%opts, 'help|?', 'verbose') or usage();
+  GetOptions(\%opts, 'help|?', 'blog=i', 'verbose') or usage();
   usage() if delete $opts{help};
 
-  usage('missing required DB file') if @ARGV == 0;
-  usage('received more than a single DB file') if @ARGV > 1;
-  usage("file '$opts{db}' not found") unless -e ($opts{db} = $ARGV[0]);
+  usage('missing required parameters: DB File, Hugo Directory') if @ARGV != 2;
+  usage("file '$opts{db}' not found")       unless -e ($opts{db}       = $ARGV[0]);
+  usage("path '$opts{hugo_dir}' not found") unless -e ($opts{hugo_dir} = $ARGV[1]);
+  usage("path '$opts{hugo_dir}' is not a directory") unless -d $opts{hugo_dir};
 
-  return;
+  return %opts;
 }
 
 
@@ -71,4 +125,9 @@ sub parse_options {
 sub debug {
   return unless $opts{verbose};
   print STDERR "[DEBUG] @_\n";
+}
+
+sub fatal {
+  print STDERR "[FATAL] @_\n";
+  exit(1);
 }
